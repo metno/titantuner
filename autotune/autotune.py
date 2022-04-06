7985001531c37497ceae1fc7a9d6986608a8e239
Ingrid Rebecca Abraham <ingridra@met.no>
2022-04-06 11:41:23 +0200

    adapt scoring for netatmo precipitation data

--- autotune/autotune.py
+++ autotune/autotune.py
@@ -4,6 +4,9 @@ import titanlib
 import random
 from typing import Callable, Union, Tuple
 import numpy as np
+import sys
+
+# from functools import reduce
 
 # typedefs
 numeric = Union[int, float]
@@ -72,30 +75,61 @@ def cost_function(hit_rate: float, false_alarm_rate: float, m: float) -> float:
     return 1 - hit_rate + (m * false_alarm_rate)
 
 
+def read_QCed_netatmo_data(filename: str) -> Tuple[titanlib.Points, np.ndarray]:
+    lats = []
+    lons = []
+    elevs = []
+    values = []
+
+    with open(filename) as f:
+        next(f)  # skip first line (titles)
+        for line in f:
+            fields = line.split(";")
+
+            prid = int(fields[4])
+            qcflag = int(fields[5])
+            if prid == 3 and qcflag == 0:
+                lats.append(float(fields[0]))
+                lons.append(float(fields[1]))
+                elevs.append(float(fields[2]))
+                values.append(float(fields[3]))
+
+    return titanlib.Points(lats, lons, elevs), values
+
+
+def precipitation_errorfunc(error: int, value: float) -> float:
+    if error != 0:
+        if value != 0:
+            return 0
+        else:
+            return 1
+    return value
+
+
 def main():
-    locations = titanlib.Points([60, 60.1, 60.2], [10, 10, 10], [0, 0, 0])
-    values = [0, 1, 1]
+    # locations = titanlib.Points([60, 60.1, 60.2], [10, 10, 10], [0, 0, 0])
+    # values = [0, 1, 1]
+    locations, values = read_QCed_netatmo_data(sys.argv[1])
 
-    errors = gen_errors(values, len(values) // 2)
-    seeded_values = seed_errors(
-        values, errors, lambda error, value: value - (error * 112)
-    )
+    errors = gen_errors(values, len(values) // 10)
+    seeded_values = seed_errors(values, errors, precipitation_errorfunc)
 
-    print("seeded_values: ", seeded_values)
+    # print("seeded_values: ", seeded_values)
 
     results = titanlib.buddy_check(
         locations,
         seeded_values,
-        [50000],
+        [5000],
         [2],
-        2,
+        0.5,
         200,
         0,
         1,
         2,
     )
 
-    print("results: ", results)
+    # print("results: ", results)
+    # print("flag_count: ", reduce(lambda x, y: x + y, results))
 
     hit_rate, false_alarm_rate = calc_hit_FR_rates(results, errors)
 
